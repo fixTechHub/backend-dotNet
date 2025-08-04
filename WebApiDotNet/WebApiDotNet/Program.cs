45b253d79c0ebb4d0e51c817c9ef715ea5e5529e
dxtuptown <mailcuakhanhdat@gmail.com>
2025-08-04 11:17:14 +0700

    #UpdateSwaggerUXUI

--- WebApiDotNet/WebApiDotNet/Program.cs
+++ WebApiDotNet/WebApiDotNet/Program.cs
@@ -5,7 +5,7 @@ using WebApiDotNet.Repository.IRepository;
 using WebApiDotNet.Repository;
 using WebApiDotNet.Services;
 using WebApiDotNet.Middleware;
-
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -67,7 +67,35 @@ builder.Services.AddCors(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApiDotNet", Version = "v1" });
+    
+    // Cấu hình authorization cho Swagger
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 var app = builder.Build();
 
