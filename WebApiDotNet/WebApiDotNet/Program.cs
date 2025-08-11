61889435f8e200e25437789ad03a437adb6efd3a
dxtuptown <mailcuakhanhdat@gmail.com>
2025-08-11 10:53:04 +0700

    #UpdateStableGate

--- WebApiDotNet/WebApiDotNet/Program.cs
+++ WebApiDotNet/WebApiDotNet/Program.cs
@@ -121,7 +121,8 @@ builder.Services.AddCors(options =>
                 "http://localhost:5173",
                 "http://localhost:5174",
                 "http://localhost:3001",
-                "https://front-pcw3xl50l-kds-projects-ce66334c.vercel.app"
+                "https://front-pcw3xl50l-kds-projects-ce66334c.vercel.app",
+                "https://front-end-git-dat-kds-projects-ce66334c.vercel.app"
             )
             .AllowAnyHeader()
             .AllowAnyMethod()
