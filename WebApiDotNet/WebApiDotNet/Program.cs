2ced54c32dd9bbcac869b9628a6c682c423dcd69
dxtuptown <mailcuakhanhdat@gmail.com>
2025-08-11 12:58:12 +0700

    #FixingBug2

--- WebApiDotNet/WebApiDotNet/Program.cs
+++ WebApiDotNet/WebApiDotNet/Program.cs
@@ -122,7 +122,7 @@ builder.Services.AddCors(options =>
                 "http://localhost:5174",
                 "http://localhost:3001",
                 "https://front-pcw3xl50l-kds-projects-ce66334c.vercel.app",
-                "https://front-end-git-dat-kds-projects-ce66334c.vercel.app",
+                "https://front-end-git-dat-kds-projects-ce66334c.vercel.app"
                 
             )
             .AllowAnyHeader()
